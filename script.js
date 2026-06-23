18a9a4d17d20bf3feeb011ab12861b1608a66e54
channarongchuathong <channarongchuathong@gmail.com>
2026-06-23 22:22:48 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id: 1,
+        name: "T-Shirt Black",
+        price: 100,
+        image: "/img/product-casual/t-shirt-black-1.png",
+        category: "casual",
+    },
+    {
+        id: 2,
+        name: "T-Shirt White",
+        price: 100,
+        image: "/img/product-casual/t-shirt-white-1.png",
+        category: "casual",
+    }
+]
+
+const productContainer = document.
