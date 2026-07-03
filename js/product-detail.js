2ffd54985a7d230808cd3287bd61a085ab0785b4
channarongchuathong <channarongchuathong@gmail.com>
2026-07-03 17:36:36 +0700

    button quantity

--- js/product-detail.js
+++ js/product-detail.js
@@ -17,6 +17,41 @@ renderProductDetail(product)
 createButtonColors(product)
 
 
+let quantity = 1
+
+let quantityDisplay = document.getElementById("quantity-shows")
+
+function renderQuantity() {
+    quantityDisplay.innerText = quantity
+}
+
+
+function changeQuantity(action) {
+
+    if (action === "increase") {
+        quantity++
+    } else if (action === "decrease") {
+        if (quantity > 1) {
+            quantity--
+        }
+    }
+    renderQuantity()
+}
+
+
+function setupQuantityEvents() {
+    const increase = document.getElementById("increase")
+    increase.addEventListener("click", () => {
+        changeQuantity("increase")
+    })
+    const decrease = document.getElementById("decrease")
+    decrease.addEventListener("click", () => {
+        changeQuantity("decrease")
+    })
+}
+
+renderQuantity()
+setupQuantityEvents()
 
 
 
--- js/render.js
+++ js/render.js
@@ -148,3 +148,5 @@ export function createButtonColors(product) {
 
     })
 }
+
+
